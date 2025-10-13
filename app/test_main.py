7f6ab85b416f1a279f10eec4ecbdab2a27cb25ae
Alejandro Chuang <alejandro.chuang@~gmail.com>
2025-10-13 14:39:28 +0200

    added test api scan in workflow2

--- app/test_main.py
+++ app/test_main.py
@@ -11,13 +11,16 @@ def test_read_main():
     assert "<title>Security Scan" in response.text  # busca texto del HTML
 
 
-
 def test_scan_url_success(monkeypatch):
     # Simula un subprocess.run exitoso
-    cp = subprocess.CompletedProcess(args=["nmap"], returncode=0, stdout="Nmap done\nPORT 22 open\n", stderr="")
+    cp = subprocess.CompletedProcess(
+        args=["nmap"], returncode=0, stdout="Nmap done\nPORT 22 open\n", stderr=""
+    )
     monkeypatch.setattr(subprocess, "run", lambda *a, **k: cp)
 
-    response = client.post("/api/scan", data={"scan_type": "url", "target": "scanme.nmap.org"})
+    response = client.post(
+        "/api/scan", data={"scan_type": "url", "target": "scanme.nmap.org"}
+    )
     assert response.status_code == 200
     # Como la vista devuelve HTML, miramos response.text
     assert "Nmap done" in response.text
